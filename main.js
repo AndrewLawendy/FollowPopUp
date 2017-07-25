b9c8b319d08c52ec0a4bd896b01eb4c1635d34e5
Andrew Hani <andro_hani@hotmail.com>
2017-07-25 08:59:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-$('.popUpBg').on('mousemove', function (e) {
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
-    var clientX = e.clientX;
-    var clientY = e.clientY;
-    var hMiddle = windowWidth / 2;
-    var vMiddle = windowHeight / 2;
-    var newYValue = ((clientX - hMiddle) / 40);
-    var newXValue = ((clientY - vMiddle) / 20);
-    $('.popUpBody').css({ transform: 'translate(-50%, -50%) rotateX(' + newXValue + 'deg) rotateY(' + newYValue + 'deg)' });
+$(document).ready(function(){
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+  var hMiddle = windowWidth / 2;
+  var vMiddle = windowHeight / 2;
+  $('.popUpBg').on('mousemove', function (e) {
+  var clientX = e.clientX;
+  var clientY = e.clientY;
+  var newYValue = ((clientX - hMiddle) / 40);
+  var newXValue = ((clientY - vMiddle) / 20);
+  $('.popUpBody').css({ transform: 'translate(-50%, -50%) rotateX(' + newXValue + 'deg) rotateY(' + newYValue + 'deg)' });
+  });
 });
